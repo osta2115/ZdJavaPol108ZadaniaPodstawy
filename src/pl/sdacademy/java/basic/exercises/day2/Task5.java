7bb158f54d97bf1c23d5230f0fd2668a7afda975
Uzytkownik <uzytkownik@kursGIT.pl>
2022-03-06 11:46:08 +0100

    day2_task5 solution

--- src/pl/sdacademy/java/basic/exercises/day2/Task5.java
+++ src/pl/sdacademy/java/basic/exercises/day2/Task5.java
@@ -2,10 +2,20 @@ package pl.sdacademy.java.basic.exercises.day2;
 
 public class Task5 {
     public static void main(String[] args) {
-
+        String input = "Ala ma kota";
+        char c = 'z';
+        System.out.println(countCharInString(input, c));
     }
 
     private static int countCharInString (String input, char searchedChar){
-        
+        int counter = 0;
+        if (StringHelper.isValid(input) && input.contains(String.valueOf(searchedChar))){
+            for (int i = input.indexOf(searchedChar); i < input.length(); i++) {
+                if (input.charAt(i) == searchedChar){
+                    counter++;
+                }
+            }
+        }
+        return counter;
     }
 }
