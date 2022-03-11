1e28465e5d36583aa33e2d0241977649b96d577e
Uzytkownik <uzytkownik@kursGIT.pl>
2022-03-11 23:08:49 +0100

    day1_task11 solution

--- src/pl/sdacademy/java/basic/exercises/day1/Task11.java
+++ src/pl/sdacademy/java/basic/exercises/day1/Task11.java
@@ -1,2 +1,28 @@
-package pl.sdacademy.java.basic.exercises.day1;public class Task11 {
+package pl.sdacademy.java.basic.exercises.day1;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Task11 {
+    public static void main(String[] args) {
+        tooMuchTooManyGame();
+    }
+
+    private static void tooMuchTooManyGame(){
+        int randomValue = new Random().nextInt(101);
+        boolean isEqual = false;
+        do {
+            Scanner scanner = new Scanner(System.in);
+            System.out.print("Insert number: ");
+            int chosenNumber = scanner.nextInt();
+            if (randomValue == chosenNumber){
+                isEqual = true;
+                System.out.println("Bingo!");
+            } else if (chosenNumber < randomValue){
+                System.out.println("Not enough");
+            } else {
+                System.out.println("Too much");
+            }
+        } while (!isEqual);
+    }
 }
